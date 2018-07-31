3f6c49c5cd5f0a560ad3acf44da08a7219ea614b
Andrew Carvajal <andrewcarvajal1@gmail.com>
2018-07-30 22:56:12 -0400

    Added button to remove all nodes

--- augmentedreality/GameViewController.swift
+++ augmentedreality/GameViewController.swift
@@ -13,14 +13,12 @@ class GameViewController: UIViewController {
     
     let arView: ARSCNView = {
         let view = ARSCNView()
-        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     let plusButtonWidth = ScreenSize.width * 0.1
     lazy var plusButton: UIButton = {
         let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.setImage(#imageLiteral(resourceName: "plus").withRenderingMode(.alwaysTemplate), for: .normal)
         button.tintColor = UIColor(red: 230, green: 145, blue: 31)
         button.layer.cornerRadius = plusButtonWidth * 0.5
@@ -32,8 +30,24 @@ class GameViewController: UIViewController {
     }()
     
     @objc func handlePlusButtonTapped() {
-        print("tap")
         addBox()
+    }
+    
+    let minusButtonWidth = ScreenSize.width * 0.1
+    lazy var minusButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "minus").withRenderingMode(.alwaysTemplate), for: .normal)
+        button.tintColor = UIColor(red: 230, green: 145, blue: 31)
+        button.layer.cornerRadius = minusButtonWidth * 0.5
+        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(handleMinusButtonTapped), for: .touchUpInside)
+        button.layer.zPosition = 1
+        button.imageView?.contentMode = .scaleAspectFill
+        return button
+    }()
+    
+    @objc func handleMinusButtonTapped() {
+        removeAllBoxes()
     }
     
     let configuration = ARWorldTrackingConfiguration()
@@ -59,13 +73,26 @@ class GameViewController: UIViewController {
         
         view.addSubview(plusButton)
         plusButton.anchor(nil, left: view.safeAreaLayoutGuide.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: nil, topConstant: 0, leftConstant: 24, bottomConstant: 12, rightConstant: 0, widthConstant: plusButtonWidth, heightConstant: plusButtonWidth)
+        
+        view.addSubview(minusButton)
+        minusButton.anchor(nil, left: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 12, rightConstant: 24, widthConstant: plusButtonWidth, heightConstant: plusButtonWidth)
     }
     
     func addBox() {
         let boxNode = SCNNode()
         boxNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0002)
         boxNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "wood")
-        boxNode.position = SCNVector3(0,0,-0.3)
+        let smallRandomFloat = Float.random(-0.5, max: 0.5)
+        boxNode.position = SCNVector3(smallRandomFloat, smallRandomFloat, smallRandomFloat)
+        boxNode.name = "box"
         arView.scene.rootNode.addChildNode(boxNode)
+    }
+    
+    func removeAllBoxes() {
+        arView.scene.rootNode.enumerateChildNodes { (node, _) in
+            if node.name == "box" {
+                node.removeFromParentNode()
+            }
+        }
     }
 }
